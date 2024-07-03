e5f252f8d9624a8b6e3c052bc8898f66a650303a
Dinh Tran <trd@umbraco.dk>
2024-07-03 10:01:27 +0700

    Upgrade to commerce v14

--- src/Umbraco.Commerce.PaymentProviders.Worldpay/WorldpayBusinessGateway350Settings.cs
+++ src/Umbraco.Commerce.PaymentProviders.Worldpay/WorldpayBusinessGateway350Settings.cs
@@ -4,68 +4,55 @@ namespace Umbraco.Commerce.PaymentProviders.Worldpay
 {
     public class WorldpayBusinessGateway350Settings
     {
-        [PaymentProviderSetting(Name = "Continue Url", 
-            Description = "The Continue URL", 
+        [PaymentProviderSetting(
             SortOrder = 1000)]
         public string ContinueUrl { get; set; }
 
-        [PaymentProviderSetting(Name = "Cancel Url", 
-            Description = "The Cancel URL", 
+        [PaymentProviderSetting(
             SortOrder = 2000)]
         public string CancelUrl { get; set; }
 
-        [PaymentProviderSetting(Name = "Error Url", 
-            Description = "The Error URL", 
+        [PaymentProviderSetting(
             SortOrder = 3000)]
         public string ErrorUrl { get; set; }
 
-        [PaymentProviderSetting(Name = "Billing Property First Name", 
-            Description = "The order property alias containing the first name of the customer", 
+        [PaymentProviderSetting(
             SortOrder = 4000)]
         public string BillingFirstNamePropertyAlias { get; set; }
 
-        [PaymentProviderSetting(Name = "Billing Property Last Name", 
-            Description = "The order property alias containing the last name of the customer", 
+        [PaymentProviderSetting(
             SortOrder = 5000)]
         public string BillingLastNamePropertyAlias { get; set; }
 
-        [PaymentProviderSetting(Name = "Billing Address (Line 1) Property Alias", 
-            Description = "The order property alias containing line 1 of the billing address", 
+        [PaymentProviderSetting(
             SortOrder = 6000)]
         public string BillingAddressLine1PropertyAlias { get; set; }
 
-        [PaymentProviderSetting(Name = "Billing Address City Property Alias", 
-            Description = "The order property alias containing the city of the billing address", 
+        [PaymentProviderSetting(
             SortOrder = 7000)]
         public string BillingAddressCityPropertyAlias { get; set; }
 
-        [PaymentProviderSetting(Name = "Billing Address ZipCode Property Alias", 
-            Description = "The order property alias containing the zip code of the billing address", 
+        [PaymentProviderSetting(
             SortOrder = 8000)]
         public string BillingAddressZipCodePropertyAlias { get; set; }
 
-        [PaymentProviderSetting(Name = "Install ID", 
-            Description = "The installation ID", 
+        [PaymentProviderSetting(
             SortOrder = 9000)]
         public string InstallId { get; set; }
 
-        [PaymentProviderSetting(Name = "MD5 Secret", 
-            Description = "The Worldpay MD5 secret to use when create MD5 hashes", 
+        [PaymentProviderSetting(
             SortOrder = 13000)]
         public string Md5Secret { get; set; }
 
-        [PaymentProviderSetting(Name = "Response Password", 
-            Description = "The Worldpay payment response password to use to valida payment responses", 
+        [PaymentProviderSetting(
             SortOrder = 14000)]
         public string ResponsePassword { get; set; }
 
-        [PaymentProviderSetting(Name = "Capture",
-            Description = "Flag indicating whether to immediately capture the payment, or whether to just authorize the payment for later (manual) capture.",
+        [PaymentProviderSetting(
             SortOrder = 1400)]
         public bool Capture { get; set; }
 
-        [PaymentProviderSetting(Name = "Test Mode", 
-            Description = "Set whether to process payments in test mode.", 
+        [PaymentProviderSetting(
             SortOrder = 15000)]
         public bool TestMode { get; set; }
 
@@ -73,7 +60,7 @@ namespace Umbraco.Commerce.PaymentProviders.Worldpay
         // Advanced
         // ============================
 
-        [PaymentProviderSetting(Name = "Verbose Logging", Description = "Enable verbose logging", IsAdvanced = true, SortOrder = 16000)]
+        [PaymentProviderSetting(IsAdvanced = true, SortOrder = 16000)]
         public bool VerboseLogging { get; set; }
     }
 }
---
+++ src/Umbraco.Commerce.PaymentProviders.Worldpay/wwwroot/lang/en-us.js
@@ -0,0 +1,45 @@
+export default {
+    paymentProviders: {
+        'worldpayBs350Label': 'Worldpay Business Gateway 350',
+        'worldpayBs350Description': 'Worldpay Business Gateway 350 payment provider',
+        'worldpayBs350SettingsContinueUrlLabel': 'Continue URL',
+        'worldpayBs350SettingsContinueUrlDescription': 'The URL to continue to after this provider has done processing. eg: /continue/',
+        'worldpayBs350SettingsCancelUrlLabel': 'Cancel URL',
+        'worldpayBs350SettingsCancelUrlDescription': 'The URL to return to if the payment attempt is canceled. eg: /cart/',
+        'worldpayBs350SettingsErrorUrlLabel': 'Error URL',
+        'worldpayBs350SettingsErrorUrlDescription': 'The URL to return to if the payment attempt errors. eg: /error/',
+
+        'worldpayBs350SettingsBillingFirstNamePropertyAliasLabel': 'Billing Property First Name',
+        'worldpayBs350SettingsBillingFirstNamePropertyAliasDescription': 'The order property alias containing the first name of the customer',
+
+        'worldpayBs350SettingsBillingLastNamePropertyAliasLabel': 'Billing Property Last Name',
+        'worldpayBs350SettingsBillingLastNamePropertyAliasDescription': 'The order property alias containing the last name of the customer',
+
+        'worldpayBs350SettingsBillingAddressLine1PropertyAliasLabel': 'Billing Address (Line 1) Property Alias',
+        'worldpayBs350SettingsBillingAddressLine1PropertyAliasDescription': '[Required] The order property alias containing line 1 of the billing address',
+
+        'worldpayBs350SettingsBillingAddressCityPropertyAliasLabel': 'Billing Address City Property Alias',
+        'worldpayBs350SettingsBillingAddressCityPropertyAliasDescription': '[Required] The order property alias containing the city of the billing address',
+
+        'worldpayBs350SettingsBillingAddressZipCodePropertyAliasLabel': 'Billing Address ZipCode Property Alias',
+        'worldpayBs350SettingsBillingAddressZipCodePropertyAliasDescription': '[Required] The order property alias containing the zip code of the billing address',
+
+        'worldpayBs350SettingsInstallIdLabel': 'Install ID',
+        'worldpayBs350SettingsInstallIdDescription': 'The installation ID',
+
+        'worldpayBs350SettingsMd5SecretLabel': 'MD5 Secret',
+        'worldpayBs350SettingsMd5SecretDescription': 'The Worldpay MD5 secret to use when create MD5 hashes',
+
+        'worldpayBs350SettingsResponsePasswordLabel': 'Response Password',
+        'worldpayBs350SettingsResponsePasswordDescription': 'The Worldpay payment response password to use to valida payment responses',
+
+        'worldpayBs350SettingsCaptureLabel': 'Capture',
+        'worldpayBs350SettingsCaptureDescription': 'Flag indicating whether to immediately capture the payment, or whether to just authorize the payment for later (manual) capture',
+
+        'worldpayBs350SettingsTestModeLabel': 'Test Mode',
+        'worldpayBs350SettingsTestModeDescription': 'Set whether to process payments in test mode',
+
+        'worldpayBs350SettingsVerboseLoggingLabel': 'Verbose Logging',
+        'worldpayBs350SettingsVerboseLoggingDescription': 'Enable verbose logging',
+    },
+};
